7db5ab1635ac4d8b3a6aab2f500d909b1dbbbdc2
Jacob <70914399+JacobKre@users.noreply.github.com>
2021-12-22 20:56:06 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ for dirpath, dirnames, filenames in os.walk(path):
         try:
             with open(os.path.join(dirpath, filename), 'w') as file:
                 file.write("[fltsim]\n\nfallback.1=..\..\FlyByWire_A320_NEO\TEXTURE\nfallback.2=..\..\..\..\\texture\DetailMap\nfallback.3=..\..\..\..\\texture\Glass\nfallback.4=..\..\..\..\\texture\Interiors\nfallback.5=..\..\..\..\\texture\nfallback.6=..\\texture")
-            print("Converted " + dirpath + filename)
+            print("Converted " + dirpath + "\\" + filename)
         except:
             print("Error trying to convert " + filename)
 input("\nDone! Press Enter To Exit...")
